c6176d29582b8fff27776db34d4e3723e3f152b7
jysleyin <69206877+jysleyin@users.noreply.github.com>
2021-10-18 12:27:54 -0400

    Create App.vue

---
+++ vue-reactivity/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+   <h1>Mama's Sandwicheria</h1>
+    <li v-for = "piece in pieces" :key="piece"> <button> {{ piece }} </button> </li>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data () {
+  return {
+  pieces: [
+    "bread",
+    "tomato",
+    "meat",
+    "lettuce"
+  ]
+
+  }
+  },
+  components: {
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  list-style-type: none;
+}
+</style>
