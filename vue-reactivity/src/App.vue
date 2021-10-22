3ecb46d357b28c972ffdc7c3b1fad35b7dc131c3
jysleyin <69206877+jysleyin@users.noreply.github.com>
2021-10-22 12:29:01 -0400

    Update App.vue

--- vue-reactivity/src/App.vue
+++ vue-reactivity/src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div name="app">
+  <div id="app">
    <h1>Mama's Sandwicheria</h1>
-    <li v-for = "(piece,index) in pieces" v-bind:key="piece.id" @click="addClick()"> <button> {{ piece.name }} </button> </li>
-    <div class="list"></div>
+    <div v-for = "(piece,index) in pieces" v-bind:key="index" @click="addClick()"> <button> {{ piece.name }} </button> </div>
+    <div id="food"><img :src= pieces.imgURL></div>
     <img src="./assets/BREAD.png">
   </div>
 </template>
@@ -14,42 +14,50 @@ export default {
   name: 'App',
   data () {
   return {
-    selectedPiece: 0,
+    // selectedPiece: 0,
   pieces: [
     {
     name: "bread",
     id: 1,
+    imgURL: require('./assets/BREAD.png'),
     },
     {
     name: "tomato",
     id: 2,
+    imgURL: require('./assets/tomato.png')
     },
     {
     name: "meat",
     id: 3,
+    imgURL: require('./assets/PATTY.png')
     },
     {
     name: "lettuce",
     id: 4,
+    imgURL: require('./assets/LETTUCE.png')
     },
     {
     name: "cheese",
     id: 5,
+    imgURL: require('./assets/CHEESE.png')
     },
     {
     name: "sauce",
-    id: 6
+    id: 6,
+    imgURL: require('./assets/SAUCE.png')
     }
   ]
 
   }
   },
   methods: {
-    updateProduct(index){
-    this.selectedPiece = index;
-    },
+    // updateProduct(index){
+    // this.selectedPiece = index;
+    // },
+    
     addClick: function() {
-      alert('filler')
+      
+      
     }
   },
 
@@ -60,6 +68,10 @@ export default {
 </script>
 
 <style>
+
+body {
+background-color: tan;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -68,10 +80,13 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
   list-style-type: none;
+  background-color: white;  
 
 }
 
-.list {
-    display: none;
+button {
+  font-size: 20px;
 }
+
+
 </style>
